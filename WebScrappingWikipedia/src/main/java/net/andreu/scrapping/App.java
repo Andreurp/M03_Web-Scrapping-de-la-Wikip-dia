a23e0a6641f27464ac23e33159e92b051d342564
Andreu <Andreu@192.168.15.134>
2016-02-16 16:40:06 +0100

    initial commit

---
+++ WebScrappingWikipedia/src/main/java/net/andreu/scrapping/App.java
@@ -0,0 +1,13 @@
+package net.andreu.scrapping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
