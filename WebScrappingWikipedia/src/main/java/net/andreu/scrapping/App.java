5ddb90075100b800ebc0adfa175a1bb2d2e8efa8
Andreu <Andreu@Phanton>
2016-02-17 12:12:49 +0100

    finally

--- WebScrappingWikipedia/src/main/java/net/andreu/scrapping/App.java
+++ WebScrappingWikipedia/src/main/java/net/andreu/scrapping/App.java
@@ -1,6 +1,9 @@
 package net.andreu.scrapping;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,6 +17,7 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
+    	BufferedWriter escriure = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("Llista de colors.txt"), "utf-8"));
         Document doc=Jsoup.connect("https://ca.wikipedia.org/wiki/Llista_de_colors").get();
         Elements taula = doc.select("tbody");
         Elements files = taula.select("tr");
@@ -22,7 +26,8 @@ public class App
         	Elements capsalera = files.get(i).select("th");
         	Elements celes = files.get(i).select("td");
         	
-        	System.out.println("INSERT INTO colors (nom, colorhex, red, green, blue) VALUES (“"+capsalera.text()+"”, “"+celes.get(1).text()+"”, "+celes.get(2).text()+","+celes.get(3).text()+","+celes.get(4).text()+");");
+        	escriure.write("INSERT INTO colors (nom, colorhex, red, green, blue) VALUES (“"+capsalera.text()+"”, “"+celes.get(1).text()+"”, "+celes.get(2).text()+","+celes.get(3).text()+","+celes.get(4).text()+");\n");
         }
+        escriure.close();
     }
 }
